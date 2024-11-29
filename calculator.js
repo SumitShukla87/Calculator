102a0259cef1cb35632d8dac344e31dacb1c7a43
Sumit Shukla <sumitshukla@Sumits-MacBook.local>
2024-11-29 21:12:30 +0530

    Handle newlines between numbers

--- calculator.js
+++ calculator.js
@@ -1,11 +1,10 @@
 function add(numbers) {
   if (numbers === "") return 0;
-
   if (!numbers.includes(",") && !numbers.includes("\n")) {
     return parseInt(numbers);
   }
 
-  const numArray = numbers.split(",").map((num) => parseInt(num));
+  const numArray = numbers.split(/[\n,]/).map((num) => parseInt(num));
   return numArray.reduce((sum, num) => sum + num, 0);
 }
 
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -10,4 +10,8 @@ describe("String Calculator Tests", () => {
   it("should return the sum of two numbers", () => {
     expect(add("1,5")).toBe(6);
   });
+
+  it("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
